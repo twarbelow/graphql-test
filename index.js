fab86db67e1794574d5d85aadf88d5239cf62556
Taija <taija.warbelow@gmail.com>
2021-08-17 13:52:26 -0700

    abstract resolvers into own file, create getFriend query

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import schema from './schema';
 import { graphqlHTTP } from 'express-graphql';
-import { test } from 'media-typer';
+import resolvers from './resolvers';  
 
 const app = express();
 
@@ -9,34 +9,7 @@ app.get('/', (req, res) => {
   res.send('Graphql is amazing!');
 });
 
-const friendDatabase = {};
-
-class Friend {
-  constructor(id, { firstName, lastName, gender, email }) {
-    this.id = id;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.gender = gender;
-    this.email = email;
-  }
-}
-
-const root = { 
-  friend: () => {
-  return {
-    "id": 135245342,
-    "firstName": "Taija",
-    "lastName": "Warbelow",
-    "gender": "female",
-    "email": "test@test.com"
-  }
-  },
-  createFriend: ({input}) => {
-    let id = require('crypto').randomBytes(10).toString('hex');
-    friendDatabase[id] = input;
-    return new Friend(id, input);
-  }
-};
+const root = resolvers
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
